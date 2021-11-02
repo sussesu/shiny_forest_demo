bf6a4db4502158d0405cfac68819f9beec4a458d
Susanne Suvanto <susanne.suvanto@luke.fi>
2021-11-02 17:36:58 +0000

    result visualization scripts

---
+++ vizualize_answers.R
@@ -0,0 +1,30 @@
+# install.packages('gsheet')
+library(gsheet)
+library(ggplot2)
+library(tidyverse)
+
+u <- "https://docs.google.com/spreadsheets/d/1r8Ty59HZqGG5eqJEs7-aCe0PNxndH8FtWmobUfqGfcA/edit?usp=sharing"
+
+vastaukset <- read.csv(text=gsheet2text(u, format='csv'), stringsAsFactors=FALSE, 
+         skip=1, header = FALSE, 
+         col.names = c("timestamp", "metsa", "keskiarvo", "keskihajonta"))
+
+
+df_list <-list ()
+for(i in 1:nrow(vastaukset)) {
+  df_list[[i]] <- data.frame(id = rep(i, 60),
+                             metsa = rep(vastaukset$metsa[i], 60),
+                             dbh = 1:60,
+                             density = dnorm(1:60, 
+                                             vastaukset$keskiarvo[i],
+                                             vastaukset$keskihajonta[i]))
+}
+
+final_densities <- do.call(rbind, df_list)
+
+final_densities %>%
+  ggplot(aes(dbh, density, col=factor(id))) +
+  geom_line() + 
+  facet_wrap(metsa ~ ., ncol=3) +
+  theme(legend.position = "none") +
+  xlab("Rungon läpimitta (cm)") 
