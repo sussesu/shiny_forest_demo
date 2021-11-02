0ac6835c54cda64456effb83f142dc8327c70b1e
Susanne Suvanto <susanne.suvanto@luke.fi>
2021-11-02 16:54:07 +0000

    add excersise link

--- ui.R
+++ ui.R
@@ -4,27 +4,29 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Luo oma metsäsi!"),
+  h1("Luo oma metsäsi"),
   helpText("Määritä puiden koko ja määrä."),
+  a(href="https://forms.gle/Nrf7w4kWoVo8JU2v5", "Tee tehtävä tästä linkistä!"),
+  hr(),
   
   
   # Sidebar with a slider input for number of observations
   sidebarPanel(
     sliderInput("mu", 
-                "Keskiarvo:", 
+                "Keskiarvo (cm):", 
                 min = 1,
                 max = 50, 
                 value = 30),
     # helpText("Määrittää puiden kokojakauman keskiarvon."),
     sliderInput("sigma", 
-                "Keskihajonta:", 
+                "Keskihajonta (cm):", 
                 min = 1,
                 max = 15, 
                 value = 5),
     # helpText("Määrittää kokojakauman keskihajonnan."),
     
     sliderInput("obs", 
-                "Puiden määrä (kpl/ha):", 
+                "Puiden määrä (kpl/hehtaari):", 
                 min = 1,
                 max = 1000, 
                 value = 500),
